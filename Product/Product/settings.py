be04515299c2c269cb545827a2775c4a55371082
Rahul jha <RAHUL23082001JHA@gmail.com>
2024-09-27 21:09:01 +0530

    Product V1

--- Product/Product/settings.py
+++ Product/Product/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = ["apiv1",
+    "rest_framework",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,12 +74,34 @@ WSGI_APPLICATION = 'Product.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES =  {
+#         'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': f'{os.environ("DATABASE_NAME")}',
+#         'USER': f"{os.environ("DATABASE_USER")}",
+#         'PASSWORD': f"{os.environ("DATABASE_PASSWORD")}",
+#         'HOST': 'product_db',
+#         'PORT': '5432',  # Default PostgreSQL port
+#     }
+# }
+DATABASES =  {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'product',
+        'USER': 'postgres',
+        'PASSWORD': 'rahul',
+        'HOST': 'localhost',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
+
+#{
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
